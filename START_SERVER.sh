27b79ff7f2743279e27de84da8d59c93030f57ec
Matej398 <matej.resman@gmail.com>
2025-11-10 18:45:24 +0100

    Remove local development server scripts (START_SERVER.bat and START_SERVER.sh)

--- START_SERVER.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Starting PHP Development Server..."
-echo ""
-echo "Server will be available at: http://localhost:8000"
-echo ""
-echo "Press Ctrl+C to stop the server"
-echo ""
-php -S localhost:8000
-
